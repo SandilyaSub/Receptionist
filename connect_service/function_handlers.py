61378549e4e4bc18e25dc7687c16696300f0c317
Sandilya Konduri <sandilya@Sandilyas-MacBook-Air.local>
2025-08-28 01:27:33 +0530

    Database operations for Call Handover 28Aug 0130

--- connect_service/function_handlers.py
+++ connect_service/function_handlers.py
@@ -112,7 +112,7 @@ class FunctionHandlers:
             # If we have a session, send a response to the user
             if self.session:
                 response_text = f"I'm transferring your call to our {department} who can better assist you with this. Please hold while I connect you."
-                await self.session.send_client_content(turns={"parts": [{"text": response_text}]})
+                await self.session.gemini_session.send_client_content(turns={"parts": [{"text": response_text}]})
                 
                 # Set the call as ended to stop the audio loop
                 self.call_ended = True
